ff5435153585440629d273af1b0a505c340f31ec
Aliaksei Ramanouski <a.romanovskiy94@gmail.com>
2026-09-26 00:49:09 +0300

    feat: add first training file

---
+++ src/HelloQA.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class HelloQA {
+    public static void main(String[] args) {
+        System.out.println("Привет, будущий SDET");
+        System.out.println(9/4);
+        System.out.println(9%4);
+    }
+}
